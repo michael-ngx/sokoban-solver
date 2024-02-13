1ee82fcf7f76c7afadeec700f1aeaa48ba8f6993
michael-ngx <nguyenvietminh812@gmail.com>
2024-02-12 19:06:06 -0500

    added some heur

--- solution.py
+++ solution.py
@@ -16,13 +16,12 @@ from sokoban import sokoban_goal_state, SokobanState, Direction, PROBLEMS  # for
 def cornered(state, box):
     '''Checks if box is stuck in corner, but not at goal state'''
 
-    up_block = (box[1] == 0) or ((box[0], box[1] + 1) in state.obstacles) 
-    down_block = (box[1] == state.height - 1) or ((box[0], box[1] - 1) in state.obstacles) 
+    up_blocked = (box[1] == 0) or ((box[0], box[1] + 1) in state.obstacles)
+    down_blocked = (box[1] == state.height - 1) or ((box[0], box[1] - 1) in state.obstacles) 
+    left_blocked = (box[0] == 0) or ((box[0] - 1, box[1]) in state.obstacles)
+    right_blocked = (box[0] == state.width - 1) or ((box[0] + 1, box[1]) in state.obstacles)
 
-    left_block = (box[0] == 0) or ((box[0] - 1, box[1]) in state.obstacles)
-    right_block = (box[0] == state.width - 1) or ((box[0] + 1, box[1]) in state.obstacles)
-
-    return (up_block or down_block) and (left_block or right_block) 
+    return (up_blocked or down_blocked) and (left_blocked or right_blocked) 
 
 ####################################################################
 ######################## SOKOBAN HEURISTICS ########################
@@ -37,7 +36,32 @@ def heur_alternate(state):
     # Your function should return a numeric value for the estimate of the distance to the goal.
     # EXPLAIN YOUR HEURISTIC IN THE COMMENTS. Please leave this function (and your explanation) at the top of your solution file, to facilitate marking.
     
-    return heur_manhattan_distance(state)  # CHANGE THIS
+    result = 0
+    for box in state.boxes:
+        if box in state.storage:
+            continue
+        
+        # Check if box is stuck in corner
+        if cornered(state, box):
+            return float('inf')
+
+        # Find the closest storage point for the box
+        min_distance = float('inf')
+        for storage in state.storage:
+            distance = abs(box[0] - storage[0]) + abs(box[1] - storage[1])
+            if distance < min_distance:
+                min_distance = distance
+        result += min_distance
+        
+        # Find the closest robot to the box
+        min_distance = float('inf')
+        for robot in state.robots:
+            distance = abs(box[0] - robot[0]) + abs(box[1] - robot[1])
+            if distance < min_distance:
+                min_distance = distance
+        result += min_distance
+    
+    return result
 
 def heur_zero(state):
     '''Zero Heuristic can be used to make A* search perform uniform cost search'''
@@ -112,14 +136,16 @@ def iterative_astar(initial_state, heur_fn, weight=1, timebound=5):  # uses f(n)
     best_stat = False
     costbound = (float('inf'), float('inf'), float('inf'))
     
-    while os.times()[0] < endtime:
-        goal, stat = se.search(timebound, costbound)
+    timeleft = endtime - os.times()[0]
+    while timeleft > 0:
+        goal, stat = se.search(timeleft, costbound)
         if goal and (goal.gval < costbound[2]):
             costbound = (float('inf'), float('inf'), goal.gval)
             best_goal = goal
             best_stat = stat
         weight *= 0.8
         se.fval_function = (lambda sN: fval_function(sN, weight))
+        timeleft = endtime - os.times()[0]
     
     return best_goal, best_stat
 
@@ -137,12 +163,14 @@ def iterative_gbfs(initial_state, heur_fn, timebound=5):  # only use h(n)
     best_stat = False
     costbound = (float('inf'), float('inf'), float('inf'))
     
-    while os.times()[0] < endtime:
-        goal, stat = se.search(timebound, costbound)
+    timeleft = endtime - os.times()[0]
+    while timeleft > 0:
+        goal, stat = se.search(timeleft, costbound)
         if goal and (goal.gval < costbound[0]):
             costbound = (goal.gval, float('inf'), float('inf'))
             best_goal = goal
             best_stat = stat
+        timeleft = endtime - os.times()[0]
     
     return best_goal, best_stat
 
